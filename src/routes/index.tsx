7ff0ca86abca7fd43630bd69a09bd41088593f9b
Kauana <kauanaagostini@gmail.com>
2025-03-16 15:31:05 -0300

    feat: create auth routes.

---
+++ src/routes/index.tsx
@@ -0,0 +1,19 @@
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
+
+import { AuthRoutes } from './auth.routes'
+import { gluestackUIConfig } from '../../config/gluestack-ui.config'
+import { Box } from '@gluestack-ui/themed'
+import { AppRoutes } from './app.routes'
+
+export function Routes() {
+  const theme = DefaultTheme
+  theme.colors.background = gluestackUIConfig.tokens.colors.background
+
+  return (
+    <Box flex={1} bg="$shape">
+      <NavigationContainer theme={theme}>
+        <AppRoutes />
+      </NavigationContainer>
+    </Box>
+  )
+}
